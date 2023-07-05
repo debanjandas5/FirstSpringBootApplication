81cdaaefab177e7b75798e4f40e4604dc2f4bf45
debanjandas5 <138683170+debanjandas5@users.noreply.github.com>
2023-07-05 22:10:43 +0530

    Update SpringbootFirstAppApplicationsd
    test commit

--- src/main/java/com/debanjan/springbootfirstapp/SpringbootFirstAppApplication.java
+++ src/main/java/com/debanjan/springbootfirstapp/SpringbootFirstAppApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootFirstAppApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Starting my first SpringBoot Application");
 		SpringApplication.run(SpringbootFirstAppApplication.class, args);
 	}
 
